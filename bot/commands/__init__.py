eb15f0188d92453232511350ed2584217ceef65c
Alejandro Pimentel <apimentel@uc.cl>
2023-01-22 16:27:25 -0300

    chore: Setup pre-commit and fix warnings

--- bot/commands/__init__.py
+++ bot/commands/__init__.py
@@ -2,3 +2,10 @@ from .add_option import AddOption
 from .new_poll import NewPoll
 from .show_options import ShowOptions
 from .start import Start
+
+COMMANDS = {
+    "add_option": AddOption,
+    "new_poll": NewPoll,
+    "show_options": ShowOptions,
+    "start": Start,
+}
